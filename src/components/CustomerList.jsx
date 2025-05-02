e29061d5f30c1b0e60ae944aa063c974880caabe
kkivilahti <bhm585@myy.haaga-helia.fi>
2025-05-02 13:12:35 +0300

    csv export for customer data

--- src/components/CustomerList.jsx
+++ src/components/CustomerList.jsx
@@ -1,7 +1,7 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { AllCommunityModule, ModuleRegistry, themeMaterial } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
 import AddCustomer from './AddCustomer';
 import EditCustomer from './EditCustomer';
@@ -9,6 +9,7 @@ import AlertMessage from "./AlertMessage";
 import Delete from './Delete';
 import { deleteCustomer } from '../api/customersApi';
 import { editCustomer } from '../api/customersApi';
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 export default function CustomerList() {
@@ -50,14 +51,32 @@ export default function CustomerList() {
     const defaultColDef = {
         sortable: true,
         filter: true,
-        resizable: true,
+        resizable: true
     };
+
+    const gridRef = useRef(null);
+    const onGridReady = params => {
+        gridRef.current = params.api;
+    };
+
+    // export grid data as csv
+    const exportData = () => {
+        gridRef.current.exportDataAsCsv({
+            fileName: 'customers.csv',
+            suppressQuotes: true,
+            columnSeparator: ';',
+        });
+    }
 
     return (
         <>
             <Typography variant="h1">Customers</Typography>
 
             <AddCustomer loadCustomers={loadCustomers} />
+
+            <Button sx={{ marginBottom: 3, marginLeft: 1 }} variant="contained" color="secondary" onClick={exportData}>
+                Export data <FileDownloadOutlinedIcon sx={{ mb: 0.5, ml: 0.5, fontSize: 20 }} />
+            </Button>
 
             <Box className="center">
                 <Box className="table-container">
@@ -66,6 +85,7 @@ export default function CustomerList() {
                         rowData={customers}
                         columnDefs={colDefs}
                         defaultColDef={defaultColDef}
+                        onGridReady={onGridReady}
                     />
                 </Box>
             </Box>
